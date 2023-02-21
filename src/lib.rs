d3ec8930d77b79feb0d8837a7fb197422788778d
mailmindlin <mailmindlin@users.noreply.github.com>
2023-02-21 15:17:26 -0500

    Bugfix

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-#![feature(box_syntax, int_roundings, new_uninit)]
+#![feature(box_syntax, int_roundings, new_uninit, unwrap_infallible, vec_into_raw_parts, const_trait_impl, slice_flatten)]
 mod apriltag_math;
 mod families;
 mod detector;
 mod util;
-// mod apriltag;
 mod quickdecode;
 pub(crate) mod quad_thresh;
 pub(crate) mod ffi;
