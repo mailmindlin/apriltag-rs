521f57d699a94ce3ed4a4a6b861c12a5bc9379ba
mailmindlin <mailmindlin@users.noreply.github.com>
2023-02-21 15:06:08 -0500

    Add more SafeZero types

--- src/util/mem.rs
+++ src/util/mem.rs
@@ -1,4 +1,4 @@
-
+/// Marker trait for types that zeroed-out memory is a valid representation
 pub(crate) trait SafeZero {}
 
 impl SafeZero for u8 {}
@@ -8,7 +8,10 @@ impl SafeZero for f32 {}
 impl SafeZero for f64 {}
 impl SafeZero for usize {}
 
+/// Arrays of SafeZero can be allocated
 impl<T: SafeZero, const N: usize> SafeZero for [T; N] {}
+//TODO: double check this is valid
+impl<T: SafeZero> SafeZero for Option<T> {}
 
 pub(crate) fn calloc<T: SafeZero>(size: usize) -> Box<[T]> {
     let res = Box::new_zeroed_slice(size);
