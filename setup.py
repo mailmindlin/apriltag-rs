8a0e6e217bbffc74c96ac519ee53cbbc59607bb7
mailmindlin <mailmindlin@users.noreply.github.com>
2023-04-06 15:59:17 -0400

    Build python wheel

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
-    name="apriltag-rs",
+    name="apriltag_rs",
     version="1.0",
-    rust_extensions=[RustExtension("hello_rust.hello_rust", binding=Binding.PyO3)],
-    packages=["hello_rust"],
+    rust_extensions=[
+        RustExtension(
+            target="apriltag_rs.apriltag_rs",
+            features=["cpython"],
+            binding=Binding.RustCPython
+        )
+    ],
+    packages=["apriltag_rs"],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
 )
