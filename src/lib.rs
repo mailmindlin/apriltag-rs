2ee298ca200381f9a2478edf1a485acfd77fc4bf
Liam Feehery <mailmindlin@users.noreply.github.com>
2023-06-26 17:49:02 -0400

    Fix exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-#![feature(box_syntax, int_roundings, new_uninit, unwrap_infallible, vec_into_raw_parts, const_trait_impl, slice_flatten, box_into_inner, allocator_api, maybe_uninit_slice, anonymous_lifetime_in_impl_trait)]
+#![feature(int_roundings, new_uninit, unwrap_infallible, vec_into_raw_parts, const_trait_impl, slice_flatten, box_into_inner, allocator_api, maybe_uninit_slice, anonymous_lifetime_in_impl_trait, array_chunks, pointer_byte_offsets, let_chains)]
 #![allow(non_snake_case)]
-mod apriltag_math;
 pub mod families;
 mod detector;
 pub mod util;
@@ -10,12 +9,21 @@ pub(crate) mod ffi;
 pub(crate) mod quad_decode;
 mod pose;
 mod dbg;
+mod detection;
 
 pub use util::Image;
 pub use quickdecode::AddFamilyError;
 pub use families::AprilTagFamily;
-pub use detector::{ApriltagDetector, ApriltagDetection, Detections};
-pub use quad_thresh::ApriltagQuadThreshParams;
+pub use detector::{AprilTagDetector, DetectorBuilder, DetectorBuildError, DetectError, DetectorConfig};
+pub use detection::{AprilTagDetection, Detections};
+pub use quad_thresh::AprilTagQuadThreshParams;
 pub use dbg::{TimeProfile, TimeProfileStatistics};
 
-pub use pose::{estimate_tag_pose, estimate_pose_for_tag_homography, estimate_tag_pose_orthogonal_iteration, ApriltagDetectionInfo, ApriltagPose, OrthogonalIterationResult};
+pub use pose::{
+    estimate_tag_pose,
+    estimate_pose_for_tag_homography,
+    estimate_tag_pose_orthogonal_iteration,
+    ApriltagDetectionInfo,
+    ApriltagPose,
+    OrthogonalIterationResult
+};
