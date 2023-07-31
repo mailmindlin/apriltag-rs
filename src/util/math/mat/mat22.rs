337e10b3c6a30f1cda6f56eb21197e877cbe6901
Liam Feehery <mailmindlin@users.noreply.github.com>
2023-07-31 16:38:11 -0400

    Add zero method

--- src/util/math/mat/mat22.rs
+++ src/util/math/mat/mat22.rs
@@ -2,6 +2,10 @@
 pub(crate) struct Mat22([f64; 4]);
 
 impl Mat22 {
+	pub(crate) const fn zeroes() -> Self {
+		Self([0., 0., 0., 0.])
+	}
+	
 	/// Compute matrix determinant
 	pub(crate) fn det(&self) -> f64 {
 		self.0[0] * self.0[3] - self.0[1] * self.0[2]
