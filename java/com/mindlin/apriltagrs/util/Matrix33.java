b1f000e252c22555f9aeddb3fc0ded11332e6a87
Liam Feehery <mailmindlin@users.noreply.github.com>
2023-09-05 13:19:02 -0400

    Java Matrix33 util methods

--- java/com/mindlin/apriltagrs/util/Matrix33.java
+++ java/com/mindlin/apriltagrs/util/Matrix33.java
@@ -19,4 +19,59 @@ public final class Matrix33 {
             throw new IllegalArgumentException();
         this.elements = elements.clone();
     }
+
+    public double get(int row, int col) {
+        if (col < 0 || row < 0 || col >= 3 || row >= 3)
+            throw new IllegalArgumentException();
+        return this.elements[row * 3 + col];
+    }
+
+    /**
+     * Converts a rotation matrix into a quaternion
+     * @return
+     */
+    public double[] as_quaternion() {
+        double[] decision = new double[] {
+            get(0, 0),
+            get(1, 1),
+            get(2, 2),
+            get(0, 0) + get(1, 1) + get(2, 2),
+        };
+
+        // Argmax
+        double max = decision[0];
+        int choice = 0;
+        for (int i = 1; i < decision.length; i++) {
+            if (decision[i] > max) {
+                max = decision[i];
+                choice = i;
+            }
+        }
+
+        double[] quat = new double[4];
+        if (choice == 3) {
+            quat[0] = get(2, 1) - get(1, 2);
+            quat[1] = get(0, 2) - get(2, 0);
+            quat[2] = get(1, 0) - get(0, 1);
+            quat[3] = 1 + decision[3];
+        } else {
+            int i = choice;
+            int j = (i + 1) % 3;
+            int k = (j + 1) % 3;
+
+            quat[i] = 1 - decision[3] + 2 * get(i, i);
+            quat[j] = get(j, i) + get(i, j);
+            quat[k] = get(k, i) + get(i, k);
+            quat[3] = get(k, j) - get(j, k);
+        }
+        
+        // Normalize
+        var norm = Math.sqrt((quat[0] * quat[0]) + (quat[1] * quat[1]) + (quat[2] * quat[2]) + (quat[3] * quat[3]));
+
+        double scale = (norm == 0.0) ? Double.NaN : 1. / norm;
+        for (int i = 0; i < quat.length; i++)
+            quat[i] *= scale;
+
+        return quat;
+    }
 }
